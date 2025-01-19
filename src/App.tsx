5fa679f4554f0e5536dbaa0f4c344061737d475e
jacek <jacek.konieczny9@gmail.com>
2025-01-19 13:49:03 +0100

    add collapsible description to todo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './scss/App.scss'
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <AddTodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {faChevronDown, faChevronUp, faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
 
 const TodoItem = () => {
     const [status, setStatus] = useState<string>("To Do");
+    const [descriptionVisible, setDescriptionVisible] = useState<boolean>(false);
 
     const handleStatusChange = () => {
         let nextStatus: string = "To Do";
@@ -19,23 +20,30 @@ const TodoItem = () => {
         setStatus(nextStatus);
     }
 
+    const toggleDescription = () => {
+        setDescriptionVisible(!descriptionVisible);
+    }
+
     return (
         <div className="todo-item">
-            <span className="todo-item__field todo-item__field--number">1</span>
-            <span className="todo-item__field todo-item__field--title">Lorem ipsum dolor sit.</span>
-            <span className="todo-item__field todo-item__field--description">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, provident.
-    </span>
-            <div className="todo-item__field todo-item__field--status">
-                <div className={`todo-item__button todo-item__button--status ${status.toLowerCase().replace(" ", "-")}`} onClick={handleStatusChange}>{status}</div>
+            <div className="todo-item__main">
+                <span className="todo-item__number">1</span>
+                <div className="todo-item__title">Lorem ipsum dolor sit.
+                    <FontAwesomeIcon icon={descriptionVisible ? faChevronUp : faChevronDown} size="lg" className="todo-item__toggle-description" onClick={toggleDescription} />
+                </div>
+                <div className="todo-item__status">
+                    <div className={`todo-item__status-button ${status.toLowerCase().replace(" ", "-")}`} onClick={handleStatusChange}>{status}</div>
+                </div>
+                <div className="todo-item__edit">
+                    <div className="todo-item__edit-button"><FontAwesomeIcon icon={faPenToSquare} size="lg"/></div>
+                </div>
+                <div className="todo-item__remove">
+                    <div className="todo-item__remove-button"><FontAwesomeIcon icon={faTrash} size="lg"/></div>
+                </div>
             </div>
-            <div className="todo-item__field todo-item__field--edit">
-                <div className="todo-item__button todo-item__button--edit"><FontAwesomeIcon icon={faPenToSquare} size="lg"/></div>
-            </div>
-            <div className="todo-item__field todo-item__field--remove">
-                <div className="todo-item__button todo-item__button--remove"><FontAwesomeIcon icon={faTrash} size="lg"/></div>
-            </div>
+            <div className={`todo-item__description ${descriptionVisible? "" : "visible"}`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum obcaecati quia ut.</div>
         </div>
+
     );
 };
 
