a7ac439cd73c78eda678a1e6d20db1a4e3911f16
jacek <jacek.konieczny9@gmail.com>
2025-02-20 18:41:30 +0100

    add mysql database connection and schema

---
+++ backend/database/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS react_todo;
+USE react_todo;
+
+CREATE TABLE IF NOT EXISTS todos (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status ENUM('To do', 'In Progress', 'Completed') DEFAULT 'To do'
+    );
